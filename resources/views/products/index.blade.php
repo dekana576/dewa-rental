9a1a16296b6272e40b2e21343b34a0490218275d
dekana576 <dekana576@gmail.com>
2024-06-06 09:07:47 +0800

    merubah desain product list

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -16,17 +16,17 @@
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th scope="col" class="px-6 py-3">#</th>
-                <th scope="col" class="px-6 py-3">Nama</th>
-                <th scope="col" class="px-6 py-3">Foto</th>
-                <th scope="col" class="px-6 py-3">Merek</thee>
-                <th scope="col" class="px-6 py-3">jenis</thee>
-                <th scope="col" class="px-6 py-3">kapasitas mesin</thee>
-                <th scope="col" class="px-6 py-3">tahun produksi</thee>
-                <th scope="col" class="px-6 py-3">no polisi</thee>
-                <th scope="col" class="px-6 py-3">no mesin</thee>
-                <th scope="col" class="px-6 py-3">deskripsi</th>
-                <th scope="col" class="px-6 py-3">Action</th>
+                <th scope="col" class="pe-3 py-3"> #</th>
+                <th scope="col" class="pe-3 py-3"> Nama</th>
+                <th scope="col" class="pe-3 py-3"> Foto</th>
+                <th scope="col" class="pe-3 py-3"> Merek</th>
+                <th scope="col" class="pe-3 py-3"> jenis</th>
+                <th scope="col" class="pe-3 py-3"> kapasitas mesin</th>
+                <th scope="col" class="pe-3 py-3"> tahun produksi</th>
+                <th scope="col" class="pe-3 py-3"> no polisi</th>
+                <th scope="col" class="pe-3 py-3"> no mesin</th>
+                <th scope="col" class="pe-3 py-3"> deskripsi</th>
+                <th scope="col" class="pe-3 py-3"> Action</th>
             </tr>
         </thead>
         <tbody>
@@ -61,16 +61,13 @@
                     {{ $rs->no_mesin }}
                 </td>
                 <td>
-                    {{ $rs->product_code }}
-                </td>
-                <td>
                     {{ $rs->deskripsi_kendaraan }}
                 </td>
                 <td class="w-36">
                     <div class="h-14 pt-5">
-                        {{-- <a href="{{ route('admin/products/show', $rs->id) }}" class="text-blue-800">Detail</a> | --}}
-                        {{-- <a href="{{ route('admin/products/edit', $rs->id)}}" class="text-green-800 pl-2">Edit</a> | --}}
-                        {{-- <form action="{{ route('admin/products/destroy', $rs->id) }}" method="POST" onsubmit="return confirm('Delete?')" class="float-right text-red-800"> --}}
+                        <a href="" class="text-blue-800">Detail</a> |
+                        <a href="" class="text-green-800 pl-2">Edit</a> |
+                        <form action="" method="POST" onsubmit="return confirm('Delete?')" class="float-right text-red-800">
                             @csrf
                             @method('DELETE')
                             <button>Delete</button>
