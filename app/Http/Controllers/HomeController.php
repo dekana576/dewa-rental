ca4fe015b85a71112898d0cb38e6ff9628bd6cce
dekana576 <dekana576@gmail.com>
2024-06-10 11:40:03 +0800

    manggil data di homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,7 +14,9 @@ class HomeController extends Controller
  
     public function index()
     {
-        return view('frontend.homepage');
+        $product = Product::orderBy('created_at', 'DESC')->get();
+
+        return view('frontend.homepage', compact('product'));
     }
  
     public function adminHome()
--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -60,6 +60,7 @@
   </div>
   <!-- Section-->
   <section class="py-5">
+    
     <div class="container px-4 px-lg-5 mt-5">
       <h3 class="text-center mb-5">Daftar Mobil</h3>
       <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
